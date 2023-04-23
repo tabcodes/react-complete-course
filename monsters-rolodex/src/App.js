5f17294495677c2d11f237919c6f5943e40a80e2
TAB <tab@tab.com>
2023-04-22 20:58:20 -0400

    Add faker call for users; add search filtering

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: [],
+      searchField: '',
+    };
+
+    console.log("Constructor.");
+  }
+
+  componentDidMount() {
+    console.log("Component Did Mount.");
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { users: users, filteredUsers: users };
+          },
+          () => {
+            console.log(this.state.users);
+          }
+        )
+      );
+  }
+
+  render() {
+    const filteredUsers = this.state.users.filter((user) => {
+      return user.name.toLowerCase().includes(this.state.searchField);
+    });
+
+    return (
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={(e) => {
+            const searchField = e.target.value.toLowerCase();
+
+            this.setState(
+              () => {
+                return { searchField }
+              }
+            );
+          }}
+        />
+
+        {filteredUsers.map((user) => {
+          return (
+            <div key={user.id}>
+              <h1>{user.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
